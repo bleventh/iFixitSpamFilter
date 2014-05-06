45d0ad0ef2f75e8dd6aef0642ad7c05df35a5e36
Brandon Leventhal <bleventh@gmail.com>
2014-05-06 16:04:36 -0700

    Add non ifixit url feature

--- featureCollection.py
+++ featureCollection.py
@@ -1,10 +1,26 @@
 from dataAcquisition import User
 from dataAcquisition import Wiki
+from bs4 import BeautifulSoup as bs
+import re
 
 userIDs = open('UserIDs.txt', 'r')
 features = open('feature.txt', 'w')
 
 
+def noniFixitDomains(user):
+   html = bs(user.about_rendered)
+   urlCount = 0.0
+   ifixitUrlCount = 0.0
+
+   for a in html.find_all('a', href=True):
+      if len(re.findall(r"ifixit", a['href'])):
+         ifixitUrlCount = ifixitUrlCount + 1
+      urlCount = urlCount + 1
+
+   if urlCount:
+      features.write('%s ' % (1.0 - (ifixitUrlCount/urlCount)))
+   else:
+      features.write('0 ')
 
 def reputationFeature(user):
    if user.reputation > 1:
@@ -22,6 +38,7 @@ for userID in userIDs:
    user = User(userID)
 
    #import functions for gathering here
+   noniFixitDomains(user)
    reputationFeature(user)
    badgeCount(user)
 

869d68c2038d2be5c2b1332d36e1f8e0a04d9f4b
mgfeldman <MarkFeldman@pcp061826pcs.wireless.calpoly.edu>
2014-05-06 16:04:52 -0700

    Add three functions
    aboutWordCount, summaryWordCount, numTeams. Also reformatted how
    counting works. It all goes through wordCount.

--- featureCollection.py
+++ featureCollection.py
@@ -1,32 +1,66 @@
 from dataAcquisition import User
 from dataAcquisition import Wiki
 
+ABOUT_MIN_WORD_COUNT = 20
+ABOUT_MAX_WORD_COUNT = 500
+SUMMARY_MIN_WORD_COUNT = 10
+SUMMARY_MAX_WORD_COUNT = 200
+
 userIDs = open('UserIDs.txt', 'r')
 features = open('feature.txt', 'w')
 
+# repuation = 0
+# total_badges = 0
+# about_word_count = 0
+# summary_word_count = 0
+# summary = ""
+# teams = 0
 
 
 def reputationFeature(user):
-   if user.reputation > 1:
+   reputation = user.reputation
+   if reputation > 1:
       features.write('0 ')
    else:
       features.write('1 ')
 
 def badgeCount(user):
-   if user.total_badges:
+   total_badges = user.total_badges
+   if total_badges:
       features.write('0 ')
    else:
       features.write('1 ')
 
-def wordCount(user):
-   words = len(user.about_rendered.split()) 
-   if words > 30 and words < 500:
+def wordCount(text):
+   if text:
+      return len(text.split())
+   else: 
+      return 0 
+
+def aboutWordCount(user):
+   about_word_count = wordCount(user.about_raw)
+
+   if about_word_count > ABOUT_MIN_WORD_COUNT and about_word_count < ABOUT_MAX_WORD_COUNT:
       features.write('0 ')
    else:
       features.write('1 ')
 
-def summary(user):
-   if user.summary:
+   print( "about: %s" % (about_word_count))
+
+def summaryWordCount(user):
+   summary = user.summary
+   summary_word_count = wordCount(user.summary)
+
+   if summary_word_count > SUMMARY_MIN_WORD_COUNT and summary_word_count < SUMMARY_MAX_WORD_COUNT:
+      features.write('0 ')
+   else:
+      features.write('1 ')
+
+   print( "summary: %s" % (summary_word_count))
+
+def numTeams (user):
+   teams = user.teams
+   if teams:
       features.write('0 ')
    else:
       features.write('1 ')
@@ -37,8 +71,9 @@ for userID in userIDs:
    #import functions for gathering here
    reputationFeature(user)
    badgeCount(user)
-   wordCount(user)
-   summary(user)
+   aboutWordCount(user)
+   summaryWordCount(user)
+   numTeams(user)
 
    features.write('\n')
 

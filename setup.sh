33645405062d866f814ad4f9fb9ac65eea768418
Brandon Leventhal <bleventh@gmail.com>
2014-05-24 15:47:46 -0700

    setup.sh installs all dependencies for this project

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future pip install appscript
+
+ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future pip install scikit-learn
+
+pip install termcolor
+pip install requests
+pip install beautifulsoup4
